f8ca9e5d01350421d2b3f59f57dfb0c09fffc63b
Ghazal-Z <137606685+Ghazal-Z@users.noreply.github.com>
2023-10-23 18:41:49 +0330

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://ghazal-ziadlou-portfolio.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Ghazal Ziadlou
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Ghazal-Z/weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://ghazal-ziadlou-portfolio.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ghazal Ziadlou
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Ghazal-Z/weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
