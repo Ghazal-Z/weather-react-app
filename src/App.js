e2a7ef06d0464356798c3b24f94109d24bf340af
Ghazal-Z <137606685+Ghazal-Z@users.noreply.github.com>
2023-10-28 21:18:57 +0330

    Added space to the URLs

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default function App() {
             target="_blank"
             rel="noreferrer"
           >
-            open-sourced on GitHub
+            open-sourced on GitHub{" "}
           </a>
           and{" "}
           <a
